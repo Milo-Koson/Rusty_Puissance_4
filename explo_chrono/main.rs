95886974af02398c73fcba8fb6f032f3e3192676
Mahery FONG <mahery.fong@reseau.eseo.fr>
2023-10-11 08:10:56 +0200

    MAJ explo chrono avec séparation gestion affichage

--- explo_chrono/main.rs
+++ explo_chrono/main.rs
@@ -2,21 +2,13 @@ use std::{f64::consts::PI, thread, time::Duration, sync::mpsc::channel};
 
 use macroquad::prelude::*;
 
+mod clock_window;
+
 const WINDOW_SIZE: i32 = 500;
 const WINDOW_MIDDLE: f32 = 0.;
 
-const X_TIMER_PLAYER_1: f32 = -0.8;
-const X_TIMER_PLAYER_2: f32 = 0.2;
-const Y_TIMER_PLAYERS: f32 = 0.7;
-
-const WIDTH_TIMER_PLAYERS: f32 = 0.6;
-const HEIGHT_TIMER_PLAYERS: f32 = 0.25;
-
 const START_TIME_MINUTES: f64 = 1.;
 const START_TIME_SECONDS: f64 = 2.;
-
-// Display constants
-const MARGIN_SELECTION_PLAYER: f32 = 0.03;
 
 fn window_conf() -> Conf {
     Conf {
@@ -29,28 +21,6 @@ fn window_conf() -> Conf {
     }
 }
 
-//time: f32
-fn get_params_time() -> TextParams<'static> {
-    //let rotation = time*(1./12. as f32)*2.*PI as f32;
-    let (font_size, font_scale, font_aspect) = camera_font_scale(0.15);
-    let text_params = TextParams { font_size, font_scale,  //rotation: rotation,
-        font_scale_aspect: font_aspect, color: BLACK, ..Default::default()};
-    return text_params;
-}
-
-fn get_params_players_name() -> TextParams<'static> {
-    let (font_size, font_scale, font_aspect) = camera_font_scale(0.12);
-    let text_params = TextParams { font_size, font_scale, 
-        font_scale_aspect: font_aspect, color: BLACK, ..Default::default()};
-    return text_params;
-}
-
-fn get_params_players_times() -> TextParams<'static> {
-    let (font_size, font_scale, font_aspect) = camera_font_scale(0.3);
-    let text_params = TextParams { font_size, font_scale, 
-        font_scale_aspect: font_aspect, color: BLACK, ..Default::default()};
-    return text_params;
-}
 /* 
 fn deacrease_time(current_player_minutes: *mut f64, current_player_seconds: *mut f64) {
     *current_player_minutes = *current_player_minutes - 1.;
@@ -127,6 +97,14 @@ impl Players {
     }
 }
 
+fn who_is_current_player(current_player: &Player) -> i8 {
+    if *current_player == Player::Player1 {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
 
@@ -181,12 +159,6 @@ async fn main() {
     });
 
     let mut end_game = false;
-    
-    // Display each time
-    let radius = 0.55;
-    let radius_half = radius / 2.;
-    let radius_three_quarter = radius*150./ 180.;
-    let half_size_time = 0.03;
 
     // Waiting tick thread
     let _ = tx_to_timer.send("Go");
@@ -198,19 +170,7 @@ async fn main() {
     while !end_game {
 
         // Background elements
-        clear_background(LIGHTGRAY);
-        draw_text_ex( "1", radius_half - half_size_time, -radius_three_quarter + half_size_time, get_params_time());//time_index as f32
-        draw_text_ex( "2", radius_three_quarter- half_size_time, -radius_half + half_size_time, get_params_time());
-        draw_text_ex( "3", radius- half_size_time * 2., WINDOW_MIDDLE, get_params_time());
-        draw_text_ex( "4", radius_three_quarter- half_size_time, radius_half + half_size_time, get_params_time());
-        draw_text_ex( "5", radius_half- half_size_time, radius_three_quarter + half_size_time, get_params_time());
-        draw_text_ex( "6", WINDOW_MIDDLE- half_size_time, radius + half_size_time, get_params_time());
-        draw_text_ex( "7", -radius_half- half_size_time, radius_three_quarter + half_size_time, get_params_time());
-        draw_text_ex( "8", -radius_three_quarter- half_size_time, radius_half + half_size_time, get_params_time());
-        draw_text_ex( "9", -radius- half_size_time, WINDOW_MIDDLE + half_size_time, get_params_time());
-        draw_text_ex( "10", -radius_three_quarter - half_size_time * 3., -radius_half + half_size_time, get_params_time());
-        draw_text_ex( "11", -radius_half- half_size_time * 2., -radius_three_quarter + half_size_time, get_params_time());
-        draw_text_ex( "12", WINDOW_MIDDLE- half_size_time * 2., -radius + half_size_time, get_params_time());
+        clock_window::display_bg();
 
         // DEV Simulation of player turn
         
@@ -249,44 +209,12 @@ async fn main() {
             players.current_player = Player::Player2;
         }
 
-        // Player designation
-        if players.current_player == Player::Player1 {
-            draw_rectangle(X_TIMER_PLAYER_1-MARGIN_SELECTION_PLAYER, Y_TIMER_PLAYERS-MARGIN_SELECTION_PLAYER as f32,
-                WIDTH_TIMER_PLAYERS+(MARGIN_SELECTION_PLAYER*2.) as f32, HEIGHT_TIMER_PLAYERS+(MARGIN_SELECTION_PLAYER*2.) as f32,
-                Color::from_rgba(25, 116, 44, 255));
-        } else {
-            draw_rectangle(X_TIMER_PLAYER_2-MARGIN_SELECTION_PLAYER as f32, Y_TIMER_PLAYERS-MARGIN_SELECTION_PLAYER as f32,
-                WIDTH_TIMER_PLAYERS+(MARGIN_SELECTION_PLAYER*2.) as f32, HEIGHT_TIMER_PLAYERS+(MARGIN_SELECTION_PLAYER*2.) as f32,
-                Color::from_rgba(25, 116, 44, 255));
-        }
+        // Display selection player
+        clock_window::display_selection_player(who_is_current_player(&players.current_player));
 
-        // Player 1 name
-        draw_text_ex( "P1:", -0.95, Y_TIMER_PLAYERS - 0.05, get_params_players_name());
-        draw_text_ex( "Player 1", -0.78, Y_TIMER_PLAYERS - 0.05, get_params_players_name());
-        draw_rectangle(X_TIMER_PLAYER_1, Y_TIMER_PLAYERS, WIDTH_TIMER_PLAYERS, HEIGHT_TIMER_PLAYERS, WHITE);
-        // Player 1 digital minutes 
-        draw_text_ex( &players.player1.minutes.to_string(), X_TIMER_PLAYER_1, 
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
-        // Two-points
-        draw_text_ex( &":", X_TIMER_PLAYER_1 + WIDTH_TIMER_PLAYERS*2./5., 
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
-        // Player 1 digital seconds 
-        draw_text_ex( &players.player1.seconds.to_string(), X_TIMER_PLAYER_1 + WIDTH_TIMER_PLAYERS*4./7., 
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
-
-        // Player 2 name
-        draw_text_ex( "P2:", 0.05, Y_TIMER_PLAYERS - 0.05, get_params_players_name());
-        draw_text_ex( "Player 2", 0.22, Y_TIMER_PLAYERS - 0.05, get_params_players_name());
-        draw_rectangle(X_TIMER_PLAYER_2, Y_TIMER_PLAYERS, WIDTH_TIMER_PLAYERS, HEIGHT_TIMER_PLAYERS, WHITE);
-        // Player 2 digital minutes 
-        draw_text_ex( &players.player2.minutes.to_string(), X_TIMER_PLAYER_2,  
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
-        // Two-points
-        draw_text_ex( &":", X_TIMER_PLAYER_2 + WIDTH_TIMER_PLAYERS*2./5.,  
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
-        // Player 2 digital seconds 
-        draw_text_ex( &players.player2.seconds.to_string(), X_TIMER_PLAYER_2 + WIDTH_TIMER_PLAYERS*4./7., 
-        Y_TIMER_PLAYERS + HEIGHT_TIMER_PLAYERS*4./5., get_params_players_times());
+        // Display players informations 
+        clock_window::display_players(&players.player1.minutes.to_string(), &players.player1.seconds.to_string(), 
+            &players.player2.minutes.to_string(), &players.player2.seconds.to_string());
         
         if let Ok(_) = rx_for_displayer.try_recv() {
             if players.tick() {
